c9b9ad491bc92d510dd8db2c3e082c61d3498e6c
jisshub <jissmon476@gmail.com>
2020-05-12 21:08:32 +0530

    dasdkjashfsada

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
+# import a user model,
+from django.contrib.auth.models import User
+
+
+# user model and posts model have a on - many relation
+# user has many posts - but a post belong to one user
 # Create your models here.
+
+class Posts(models.Model):
+    title = models.CharField("title", max_length=100)
+    content = models.TextField("content", max_length=200)
+    date = models.DateTimeField(default=timezone.now)
+    # set author as foreign key - relate to User model
+    # set on_delete to cascade sicne a author is deletd -all his posts should be removed
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
